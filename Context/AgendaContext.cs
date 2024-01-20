7180c1a4b5914786233d8801aa4f7b205dc4da53
Gustavo <gustavooliveiracunha812@gmail.com>
2024-01-20 17:17:19 -0300

    configs in repository and gitignore.txt deleted

--- Context/AgendaContext.cs
+++ Context/AgendaContext.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API_C_SHARP_DIO.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace API_C_SHARP_DIO.Context
 {
     public class AgendaContext : DbContext
-    {
+    {  
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
     }
 }
