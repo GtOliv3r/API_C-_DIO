50b850f158b254cc6421ffc6a1aadd48362b9386
Gustavo <gustavooliveiracunha812@gmail.com>
2024-01-20 16:27:10 -0300

    feat: Add Context Db
    Add Context Db class, Nuget Packages and settings in json development arquives for database configuration

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_C_SHARP_DIO.Context
+{
+    public class AgendaContext : DbContext
+    {
+    }
+}
